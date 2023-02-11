401348c12f168fa2e3f5c3c35295d06c9e681b48
Agnieszka Nowotna <a.ilkow7@gmail.com>
2023-02-11 14:22:28 +0100

    Add form

--- app.py
+++ app.py
@@ -1,34 +1,52 @@
-import json
 import csv
 import requests
-from flask import Flask
+from flask import Flask, request, render_template
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
 
 rates = data[0]["rates"]
 
-currency_list = []
-code_list = []
-bid_list = []
-ask_list = []
+class Currency():
+    def __init__(self, currency, code, bid, ask):
+        self.currency = currency
+        self.code = code
+        self.bid = bid
+        self.ask = ask
 
-for position in rates:
-    currency_list.append(position['currency'])
-    code_list.append(position['code'])
-    bid_list.append(position['bid'])
-    ask_list.append(position['ask'])
-
+def load_values():
+    list = []
+    for position in rates:
+        position['currency'] = Currency(position['currency'], position['code'], position['bid'], position['ask'])
+        list.append(position['currency'])
+    return list
+    
+offer = load_values()
 
 with open('currencies.csv','w',newline='',encoding="utf-8") as csvfile:
     fieldnames=["currency","code","bid","ask"]
     writer = csv.DictWriter(csvfile, delimiter =";", fieldnames = fieldnames)
     writer.writeheader()
 
-    for i in range (len(currency_list)):
-        currency = currency_list[i]
-        code = code_list[i]
-        bid = bid_list[i]
-        ask = ask_list[i]
-        writer.writerow({"currency":currency,"code":code,"bid":bid,"ask":ask})
+    for currency in offer:
+        writer.writerow({"currency":currency.currency,"code":currency.code,"bid":currency.bid,"ask":currency.ask})
 
+app = Flask(__name__)
+
+@app.route('/exchange', methods = ['GET','POST'])
+def exchange():
+    if request.method =='POST':
+
+        code = request.form['currency']
+        amount = request.form['amount']
+        
+        for instance in offer:
+            if instance.code == code:
+                cost = int(amount) * instance.ask
+
+        return f"Zakup {amount} {code} kosztuje {cost:.2f} PLN"
+
+    return render_template('currencies_exchange.html', offer = offer)
+
+if __name__=='__main__':
+    app.run(debug=True)
